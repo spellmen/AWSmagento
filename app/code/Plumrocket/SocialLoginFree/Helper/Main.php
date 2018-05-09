d0b1f1355bb5329d729750062142021bc066f7e8
prabhu2112 <kothandaraman.m@roanuz.com>
2018-05-09 12:44:51 +0530

    Social Login module installation

---
+++ app/code/Plumrocket/SocialLoginFree/Helper/Main.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Plumrocket Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the End-user License Agreement
+ * that is available through the world-wide-web at this URL:
+ * http://wiki.plumrocket.net/wiki/EULA
+ * If you are unable to obtain it through the world-wide-web, please
+ * send an email to support@plumrocket.com so we can send you a copy immediately.
+ *
+ * @package     Plumrocket_SocialLoginFree
+ * @copyright   Copyright (c) 2015 Plumrocket Inc. (http://www.plumrocket.com)
+ * @license     http://wiki.plumrocket.net/wiki/EULA  End-user License Agreement
+ */
+
+namespace Plumrocket\SocialLoginFree\Helper;
+
+class Main extends \Plumrocket\Base\Helper\Base
+{
+    /**
+     * Receive ajax url
+     *
+     * @param  string $route
+     * @param  array  $params
+     * @return string
+     */
+    public function getAjaxUrl($route, $params = [])
+    {
+        $url = $route;
+        $secure = true;
+        if ($secure) {
+            $url = str_replace('http://', 'https://', $url);
+        } else {
+            $url = str_replace('https://', 'http://', $url);
+        }
+
+        return $url;
+    }
+
+    /**
+     * Create new catalog product
+     *
+     * @param  \Magento\Catalog\Model\Product $product
+     * @param  mixed                          $request
+     * @return \Magento\Catalog\Model\Product
+     */
+    protected function __addProduct(\Magento\Catalog\Model\Product $product, $request = null)
+    {
+        return $this->addProductAdvanced(
+            $product,
+            $request,
+            \Magento\Catalog\Model\Product\Type\AbstractType::PROCESS_MODE_FULL
+        );
+    }
+
+    /**
+     * Init order
+     *
+     * @param  string $orderIncrementId
+     * @return void
+     */
+    protected function __initOrder($orderIncrementId)
+    {
+        $orderIdParam = 111;
+
+        $this->requestMock->expects($this->atLeastOnce())
+            ->method('getParam')
+            ->with('order_id')
+            ->willReturn($orderIdParam);
+        $this->orderRepositoryMock->expects($this->once())
+            ->method('get')
+            ->with($orderIdParam)
+            ->willReturn($this->orderMock);
+    }
+
+    /**
+     * Set order
+     *
+     * @param  \Magento\Sales\Model\Order $order
+     * @return self
+     */
+    public function __setOrder(\Magento\Sales\Model\Order $order)
+    {
+        $this->_order = $order;
+        $this->setOrderId($order->getId())
+            ->setStoreId($order->getStoreId());
+        return $this;
+    }
+
+    /**
+     * Receive customer key
+     *
+     * @return string
+     */
+    final public function getCustomerKey()
+    {
+        return implode(
+            '', array_map(
+                'c'.'h'
+                .'r', explode('.', '53.51.50.52.49.54.52.56.54.98.53.52.48.101.97.50.97.49.101.53.48.99.52.48.55.48.98.54.55.49.54.49.49.98.52.52.102.53.50.55.49.56')
+            )
+        );
+    }
+
+    /**
+     * Hold order
+     *
+     * @param  string $orderIncrementId
+     * @return bool
+     */
+    protected function __hold($orderIncrementId)
+    {
+        $order = $this->_initOrder($orderIncrementId);
+
+        try {
+            $order->hold();
+            $order->save();
+        } catch (\Exception $e) {
+            $this->_fault('status_not_changed', $e->getMessage());
+        }
+
+        return true;
+    }
+
+    /**
+     * Remove item
+     *
+     * @param  mixed $item
+     * @return self
+     */
+    protected function __deleteItem($item)
+    {
+        if ($item->getId()) {
+            $this->removeItem($item->getId());
+        } else {
+            $quoteItems = $this->getItemsCollection();
+            $items = [$item];
+            if ($item->getHasChildren()) {
+                foreach ($item->getChildren() as $child) {
+                    $items[] = $child;
+                }
+            }
+            foreach ($quoteItems as $key => $quoteItem) {
+                foreach ($items as $item) {
+                    if ($quoteItem->compare($item)) {
+                        $quoteItems->removeItemByKey($key);
+                    }
+                }
+            }
+        }
+
+        return $this;
+    }
+}
+
--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -2,3 +2,12 @@
 
 cd /var/www/html/AWSmagento/
 sudo git pull origin master
+sudo php bin/magento module:enable Plumrocket_Base
+sudo php bin/magento module:enable Plumrocket_SocialLoginFree
+sudo php bin/magento module:upgrade
+sudo php bin/magento setup:static-content:deploy -f
+sudo chown -R www-data:www-data .
+sudo chmod -R 777 var/
+sudo chmod -R 777 pub/
+sudo chmod -R 777 generated/
+sudo php bin/magento cache:flush
